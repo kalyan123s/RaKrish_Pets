23a198aeb5341a1bb202f1365d6fc0ddbfd3eee3
Kalyan Sharma <kalyansharma855@gmail.com>
2023-08-15 12:20:44 +0530

    website updates

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ***********************==================Toggle button=====================***************************
+
 function toggleMenu() {
     const dropdownMenu = document.getElementById('dropdownMenu');
     const menuIcon = document.querySelector('.menu i');
@@ -20,3 +22,44 @@ function toggleMenu() {
       menuIcon.classList = 'fa-solid fa-bars';
     }
   });
+
+  // **********==================Scrolling functionality with jquery===========***************************
+
+  // Option 2 - jQuery Smooth Scrolling
+$('.navbar a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body')
+      .animate({
+        scrollTop: $(hash).offset().top
+      }, 10);
+  }
+});
+
+
+// *********************========================Login Button=========================**************************
+
+const loginSignupContainer= $('.login_signup_container');
+const loginLink=$('.login-link');
+const SignupLink=$('.Signup-link');
+const loginButton = $('.login_btn');
+const CloseIcon= $('.icon-close');
+
+SignupLink.on('click', function(){
+    loginSignupContainer.addClass('active');
+});
+
+loginLink.on('click', function(){
+    loginSignupContainer.removeClass('active');
+});
+
+loginButton.on('click', function(){
+    loginSignupContainer.addClass('toggle');
+});
+
+CloseIcon.on('click',function(){
+    loginSignupContainer.removeClass('toggle');
+})
